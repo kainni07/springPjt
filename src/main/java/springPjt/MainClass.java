8a42ff6c59ae9c9ee63a54587a8577277d54eb1c
kaimi <kainni07@gmail.com>
2019-09-17 07:31:40 +0900

    TransportationWalkクラス追加

---
+++ src/main/java/springPjt/MainClass.java
@@ -0,0 +1,11 @@
+package springPjt;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		TransportationWalk tranasportationWalk = new TransportationWalk();
+		tranasportationWalk.move();
+	}
+
+}
---
+++ src/main/java/springPjt/TransportationWalk.java
@@ -0,0 +1,9 @@
+package springPjt;
+
+public class TransportationWalk {
+
+	public void move() {
+
+		System.out.println("徒歩で移動します");
+	}
+}
