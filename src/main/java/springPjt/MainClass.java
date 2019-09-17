1ee81cf0408e42ca746d0d1d9d494bb73a790c04
kaimi <kainni07@gmail.com>
2019-09-18 07:29:01 +0900

    applicationContext.xmlを追加

--- src/main/java/springPjt/MainClass.java
+++ src/main/java/springPjt/MainClass.java
@@ -1,11 +1,20 @@
 package springPjt;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 
-		TransportationWalk tranasportationWalk = new TransportationWalk();
-		tranasportationWalk.move();
+//		TransportationWalk tranasportationWalk = new TransportationWalk();
+//		tranasportationWalk.move();
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
+
+		transportationWalk.move();
+
+		ctx.close();
 	}
 
 }
